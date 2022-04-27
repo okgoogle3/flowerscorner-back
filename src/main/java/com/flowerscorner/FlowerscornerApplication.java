a566c67fed1dfbe877d089ad6200168165f147f1
okgoogle3 <halalah.han@gmail.com>
2022-04-27 14:28:52 +0300

    Initial commit

---
+++ src/main/java/com/flowerscorner/FlowerscornerApplication.java
@@ -0,0 +1,13 @@
+package com.flowerscorner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerscornerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowerscornerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flowerscorner/FlowerscornerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowerscorner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerscornerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
